25177fc3ed7696caad55e9f92402e988be07e178
tonyadms1509 <tonyadams1509@gmail.com>
2026-03-01 21:17:38 +0200

    Remove PayPalTest import and fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import React from "react"
-import PaystackTest from "./PaystackTest"
-import PayPalTest from "./PayPalTest"
+import PaystackTest from "./PaystackTest";
 
 function App() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
-      <h1>StockLink Ferrari Sandbox</h1>
-      <p>Simulate payments below to test your webhook flow.</p>
+    <div>
       <PaystackTest />
-      <hr style={{ margin: "2rem 0" }} />
-      <PayPalTest />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
