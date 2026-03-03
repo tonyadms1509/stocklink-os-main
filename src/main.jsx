02e1419024e0a2ed618b2225c28cefc980e0da7e
tonyadms1509 <tonyadams1509@gmail.com>
2026-03-03 19:33:24 +0200

    Add Vite config and fix React entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-// src/main.jsx
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'   // optional if you use Tailwind or global styles
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
