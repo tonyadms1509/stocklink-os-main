342c68ad5c7e9c9c9be7c5915acaa8254296be59
tonyadms1509 <tonyadams1509@gmail.com>
2026-04-03 13:59:08 +0200

    fix: add DealerLocationChart placeholder

---
+++ src/components/DealerLocationChart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DealerLocationChart() {
+  return (
+    <div style={{ color: "white", padding: "1rem" }}>
+      Dealer Bar Chart Placeholder
+    </div>
+  );
+}
